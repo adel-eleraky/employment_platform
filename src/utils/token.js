75010215003948ac43f4d42420ef1f871e17014b
adel eleraky <kameladel339@gmail.com>
2025-04-07 10:10:14 +0200

    feat: created save token in cookie func

---
+++ src/utils/token.js
@@ -0,0 +1,14 @@
+
+const saveTokenInCookie = (res, token) => {
+
+    const cookieOptions = {
+        expires: new Date(Date.now() + (process.env.JWT_EXPIRE_IN * 24 * 60 * 60 * 1000)),
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+    res.cookie("jwt", token, cookieOptions)
+}
+
+export default saveTokenInCookie
