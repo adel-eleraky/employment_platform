44361ad262fd4ae29573070f2f6a3bbc32fc5963
adel eleraky <kameladel339@gmail.com>
2025-04-07 10:07:02 +0200

    feat: created employer schema

---
+++ src/DB/models/employer.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose"
+
+
+const employerSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: [true, "name is required"],
+        minLength: [3, "minimum length is 3 characters"],
+        maxLength: [25, "max length is 25 characters"]
+    },
+    title: {
+        type: String,
+        required: [true, "title is required"]
+    },
+    national_ID: {
+        type: Number,
+        unique: [true, "National ID must be unique"],
+        required: [true, "national ID is required"]
+    },
+    email: {
+        type: String,
+        unique: [true, "email must be unique"],
+        required: [true, "email is required"]
+    },
+    password: {
+        type: String,
+        minLength: [8, "minimum length is 8"]
+    },
+    city: {
+        type: String,
+        required: [true, "city is required"]
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true , "company is required"],
+        ref: "Company"
+    }
+}, { timestamps: true })
+
+
+
+employerSchema.pre("save" , function(next) {
+
+    console.log("doc" , this)
+})
+
+const employerModel = mongoose.model("Employer" , employerSchema)
+
+export default employerModel
