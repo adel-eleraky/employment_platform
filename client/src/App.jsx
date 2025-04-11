f1d9678799cb1387e65ef521ea114e8dc49f21ce
adel eleraky <kameladel339@gmail.com>
2025-04-11 05:47:23 +0200

    feat: created routes for login , register , home and jobs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Login from './pages/login/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Provider } from 'react-redux';
+import store from './rtk/Store';
+import Register from './pages/register/Register';
+import UnAuthRoute from './middlewares/UnAuthRoute';
+import ProtectRoute from './middlewares/protectRoute';
+import Profile from './pages/profile/Profile';
+import Layout from './components/layout/Layout';
+import Home from './pages/home/Home';
+import JobList from './pages/jobs/Joblist';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<Layout />}>
+
+                            <Route path='/' element={<Home />} />
+                            <Route element={<UnAuthRoute />} >
+                                <Route path='login' element={<Login />} />
+                                <Route path='register' element={<Register />} />
+                            </Route>
+
+                            <Route element={<ProtectRoute />} >
+                                <Route path="profile" element={<Profile />} />
+                            </Route>
+
+                            <Route path='jobs' element={<JobList />} />
+                        </Route>
+
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </>
+    )
 }
 
 export default App
