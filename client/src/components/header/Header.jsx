ff3a987983bac240bfa3a09734beda9c93bd32dd
adel eleraky <kameladel339@gmail.com>
2025-04-11 05:48:15 +0200

    feat: created header component

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import header_img_1 from "../../assets/header-1.webp"
+import "./Header.css"
+
+function Header() {
+    return (
+        <>
+            <div className="header_section d-flex justify-content-center align-items-center">
+                <div className="content">
+                    <h1> Find your dream Job </h1>
+                    <p> Browse thousands of job opportunities and find the perfect role to match your skills </p>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
