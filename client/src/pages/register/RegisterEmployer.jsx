5a18d351bc3b32abf1df3fb94fc6f16357ed1c80
adel eleraky <kameladel339@gmail.com>
2025-04-11 05:44:45 +0200

    feat: created register employer component

---
+++ client/src/pages/register/RegisterEmployer.jsx
@@ -0,0 +1,57 @@
+import { ErrorMessage, Field, Form, Formik, useFormikContext } from "formik";
+import React from 'react'
+
+function RegisterEmployer({  touched, errors }) {
+
+    const { setFieldValue } = useFormikContext()
+
+    return (
+        <>
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <label htmlFor="company_name" className="form-label">Company Name</label>
+                    <div className="input-group has-validation mb-4">
+                        <span className="input-group-text rounded-0 rounded-start" id="basic-addon1">
+                            <i className="fa-solid fa-building"></i>
+                        </span>
+                        <Field
+                            type="text"
+                            id="company_name"
+                            name="company_name"
+                            className={`form-control ${touched.company_name && errors.company_name && "is-invalid"} rounded-0 rounded-end`}
+                            placeholder="Enter company name"
+                        />
+                        <ErrorMessage
+                            name="company_name"
+                            component="div"
+                            className="invalid-feedback d-block fs-6 fw-bold"
+                        />
+                    </div>
+                </div>
+                <div className="col-12 col-md-6">
+                    <label htmlFor="company_location" className="form-label">Company Location</label>
+                    <div className="input-group has-validation mb-4">
+                        <span className="input-group-text rounded-0 rounded-start" id="basic-addon1">
+                            <i className="fa-solid fa-location-dot"></i>
+                        </span>
+                        <Field
+                            type="text"
+                            id="company_location"
+                            name="company_location"
+                            className={`form-control ${touched.company_location && errors.company_location && "is-invalid"} rounded-0 rounded-end`}
+                            placeholder="Enter company location"
+                        />
+                        <ErrorMessage
+                            name="company_location"
+                            component="div"
+                            className="invalid-feedback d-block fs-6 fw-bold"
+                        />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default RegisterEmployer
