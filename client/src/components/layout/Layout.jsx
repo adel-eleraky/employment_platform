b9784242543d05755f7006532ac8d3e2c5ce78d6
adel eleraky <kameladel339@gmail.com>
2025-04-12 06:56:36 +0200

    feat: dispatch logged-in-user when app open

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../navbar/Navbar'
 import { Outlet } from 'react-router'
+import { useDispatch } from 'react-redux'
+import { getLoggedInUser } from '../../rtk/features/authSlice'
+import Toast from '../Toast'
 
 function Layout() {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getLoggedInUser())
+    }, [])
+
+    
     return (
         <>
             <Navbar />
             <Outlet />
+            <Toast />
         </>
     )
 }
