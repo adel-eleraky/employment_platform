533400cf46d16eb7041c5f406f8fd14ba8a2e859
adel eleraky <kameladel339@gmail.com>
2025-04-12 06:58:18 +0200

    feat: render job cards with DB data

--- client/src/pages/jobs/JobList.jsx
+++ client/src/pages/jobs/JobList.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import JobCard from '../../components/Job_card/JobCard'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllJobs, getAppliedJobs } from '../../rtk/features/jobSlice'
+import { toast } from 'react-toastify'
 
 function JobList() {
+
+    const dispatch = useDispatch()
+    const { jobs, applyMessage, applyLoading } = useSelector(state => state.job)
+    const { user } = useSelector(state => state.auth)
+    
+    const JobsElements = jobs && jobs.map(job => {
+        return (
+            <div key={job._id} className="col-12 col-md-6 col-lg-4">
+                <JobCard job={job} />
+            </div>
+        )
+    })
+
+
+    useEffect(() => {
+        dispatch(getAllJobs())
+    }, [])
+
+
+    useEffect(() => {
+
+        if (applyMessage ) {
+            toast.success(`${applyMessage}`);
+        }
+
+    }, [applyMessage])
+
+
+    useEffect(() => {
+        dispatch(getAppliedJobs())
+    }, [user])
+    
     return (
-        <div className='job_list' style={{ marginTop: "150px" }}>
+        <div className='job_list' >
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <JobCard />
-                    </div>
+                    {JobsElements}
                 </div>
             </div>
         </div>
