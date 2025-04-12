20dd6b9900d243dbf9e0ac26878394977ade9b78
adel eleraky <kameladel339@gmail.com>
2025-04-12 07:06:19 +0200

    feat: added routes for get logged-in user & logout

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from "express"
-import { login, register } from "../controllers/auth.controller.js"
+import { getLoggedInUser, login, logout, register } from "../controllers/auth.controller.js"
 import { loginSchema, registerSchema, validate } from "../middlewares/validation/auth.validation.js"
+import { AuthMiddleware } from "../middlewares/AuthMiddleware.js"
 
 
 const router = express.Router()
 
 router.post("/register" , validate(registerSchema), register)
 router.post("/login" , validate(loginSchema), login)
+router.get("/logout" , logout)
+router.get("/me" , AuthMiddleware , getLoggedInUser)
 
 export default router
