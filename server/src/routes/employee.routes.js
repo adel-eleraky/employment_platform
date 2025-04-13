761ef0a49b6f9bd3047a9eb5dced1cc16e572aa2
adel eleraky <kameladel339@gmail.com>
2025-04-13 11:32:56 +0200

    feat: added routes for views and notifications

--- server/src/routes/employee.routes.js
+++ server/src/routes/employee.routes.js
@@ -1,7 +1,14 @@
 import express from "express"
+import { AuthMiddleware, restrictTo } from "../middlewares/AuthMiddleware.js"
+import { getViews, getNotifications, updateEmployee } from "../controllers/employee.controller.js"
+import { updateEmployeeSchema, validate } from "../middlewares/validation/update.validation.js"
 
 
 const router = express.Router()
 
+router.get("/views" , AuthMiddleware , getViews)
 
+router.get("/notifications" , AuthMiddleware , getNotifications)
+
+router.put("/update" , AuthMiddleware , restrictTo("Employee"), validate(updateEmployeeSchema) , updateEmployee)
 export default router
