1cd42a414fe7426d78eecd1be77923afb06a68cb
adel eleraky <kameladel339@gmail.com>
2025-04-13 11:33:19 +0200

    feat: added routes for view employee

--- server/src/routes/employer.routes.js
+++ server/src/routes/employer.routes.js
@@ -1,7 +1,11 @@
 import express from "express"
+import { AuthMiddleware, restrictTo } from "../middlewares/AuthMiddleware.js"
+import { viewEmployee } from "../controllers/employer.controller.js"
 
 
 const router = express.Router()
 
+
+router.post("/view/:empId" , AuthMiddleware , restrictTo("Employer") , viewEmployee)
 
 export default router
