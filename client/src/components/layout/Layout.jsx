f6c44968e670c844cb39b9e4f649abcb526a906a
adel eleraky <kameladel339@gmail.com>
2025-04-13 11:27:48 +0200

    feat: dispatch notifications

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from 'react'
 import Navbar from '../navbar/Navbar'
 import { Outlet } from 'react-router'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getLoggedInUser } from '../../rtk/features/authSlice'
 import Toast from '../Toast'
+import { getUserNotifications } from '../../rtk/features/notificationSlice'
 
 function Layout() {
 
     const dispatch = useDispatch()
+    const { user } = useSelector(state => state.auth)
 
     useEffect(() => {
         dispatch(getLoggedInUser())
     }, [])
 
+    useEffect(() => {
+        dispatch(getUserNotifications())
+    }, [ user])
     
     return (
         <>
